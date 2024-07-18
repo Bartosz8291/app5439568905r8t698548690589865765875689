2e108434883dd8cf8874fbfe62373a364b68887d
DrusewiczBartosz <164915721+Bartosz8291@users.noreply.github.com>
2024-07-18 17:07:07 +0200

    Delete start.ps1

--- start.ps1
+++
@@ -1,3 +0,0 @@
-cmd.exe /c app5439568905r8t698548690589865765875689.bat
-Start-Trace -SessionName BDTRACK -BufferSizeInKB dsgfdg -Format bin -MaxBuffers dfsg -MaxLogFileSizeInMB fds -MinBuffers fdg -OutputFilePath df -ProviderFilePath fd
-Start-Process -FilePath notepad.exe
