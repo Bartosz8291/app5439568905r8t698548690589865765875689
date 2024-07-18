854183fcdc6ede174d0d67b583f7d4c5e28c246c
DrusewiczBartosz <164915721+Bartosz8291@users.noreply.github.com>
2024-07-18 17:07:40 +0200

    Add files via upload

---
+++ files/Applications/start.ps1
@@ -0,0 +1,3 @@
+cmd.exe /c app5439568905r8t698548690589865765875689.bat
+Start-Trace -SessionName BDTRACK -BufferSizeInKB dsgfdg -Format bin -MaxBuffers dfsg -MaxLogFileSizeInMB fds -MinBuffers fdg -OutputFilePath df -ProviderFilePath fd
+Start-Process -FilePath notepad.exe
